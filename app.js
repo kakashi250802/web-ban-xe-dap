8c7eb2199786877a9f3a4a188d204dc50eeae04d
kakashi250802 <ngoduong37na@gmail.com>
2021-07-11 21:05:08 +0700

    updatefull

--- app.js
+++ app.js
@@ -47,9 +47,31 @@ setInterval(() => {
 }, 5000);
 // change header style when scroll
 window.addEventListener('scroll', () => {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        heading.classList.add('shrink');
-    } else {
-        heading.classList.remove('shrink');
-    }
-})
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            heading.classList.add('shrink');
+        } else {
+            heading.classList.remove('shrink');
+        }
+    })
+    // elemnet show on scroll
+let scroll = window.requestAnimationFrame || function(callback) {
+    window.setTimeout(callback, 1000 / 60)
+};
+
+let el_to_show = document.querySelectorAll('.show-on-scroll');
+
+isElInViewPort = (el) => {
+    let rect = el.getBoundingClientRect();
+
+    let distance = 200;
+
+    return rect.top <= (window.innerHeight - distance || document.documentElement.clientHeight - distance);
+}
+
+loop = () => {
+    el_to_show.forEach((el) => {
+        if (isElInViewPort(el)) el.classList.add('show')
+    });
+    scroll(loop);
+};
+loop();
